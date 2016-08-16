00ada2e4775bfbfd004d349141ff51f46a429451
tangzx <272669294@qq.com>
2016-08-16 10:30:48 +0800

    update

---
+++ Director-Editor/Classes/Sequencer/SequencerDataInspector.cs
@@ -0,0 +1,27 @@
+﻿using Tangzx.Director;
+using UnityEditor;
+using UnityEngine;
+
+namespace TangzxInternal
+{
+    [CustomEditor(typeof(SequencerData))]
+    public class SequencerDataInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            SequencerData sd = target as SequencerData;
+
+            GUI.enabled = false;
+            for (int i = 0; i < sd.categories.Count; i++)
+            {
+                EditorGUILayout.TextField("Index" + i, sd.categories[i].categoryName);
+            }
+            
+            if (sd.defaultCategory != null)
+            {
+                EditorGUILayout.TextField("Default", sd.defaultCategory.categoryName);
+            }
+            GUI.enabled = true;
+        }
+    }
+}
